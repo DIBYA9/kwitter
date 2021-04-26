6337e5a7b883d757887f65f38ff603bfa105922e
DIBYA9 <77687274+DIBYA9@users.noreply.github.com>
2021-04-26 21:35:04 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -14,16 +14,46 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+function addroom() {
+    room_name = document.getElementById("Room").value;
+    firebase.database().ref("/").child(room_name).update({
+        purpose: "adding room name"
+    });
+    localStorage.setItem("room_name", room_name);
+    window.location = "kwitter_room.html";
+}
+
 function getData() {
     firebase.database().ref("/").on('value', function(snapshot) {
         document.getElementById("output").innerHTML = "";
         snapshot.forEach(function(childSnapshot) {
             childKey = childSnapshot.key;
             Room_names = childKey;
+            message_data = childData;
             //Start code
+            console.log("room name -" + Room_names);
+            row = "<div class='room_name' id=" + Room_names + " onclick='redirectToRoomName(this.id)' >#" + Room_names + "</div><hr>";
+            document.getElementById("output").innerHTML += row;
+
+
+            console.log(Room_names);
+            console.log(message_data);
+            name = message_data['name'];
+            message = message_data['message'];
+            like = message_data['like'];
+            name_with_tag = "<h4>" + name + "<img src='tick.png'></h4>"
+            message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+            like_button = "<button class='btn btn-warning' id=" + Room_names + " value=" + like + " onclick='updatelike()'>"
+
 
             //End code
         });
     });
 }
 getData();
+
+function redirectToRoomName(name) {
+    console.log(name);
+    localStorage.setItem("room_name", name);
+    window.location = "kwitter_page.html";
+}
